f5396091daf9a6712e3fc8a3fdfbf69d0e5604d9
Adriano <44750576+Co3Adriano@users.noreply.github.com>
2024-06-22 12:59:48 +0200

    k

--- Source/ProjectGhost/Character/PlayerAnimInstance.h
+++ Source/ProjectGhost/Character/PlayerAnimInstance.h
@@ -62,4 +62,7 @@ private:
 	
 	UPROPERTY(BlueprintReadOnly, Category  = "Movement", meta = (AllowPrivateAccess = "true") )
 	float AO_Pitch;
+
+	UPROPERTY(BlueprintReadOnly, Category  = "Movement", meta = (AllowPrivateAccess = "true") )
+	FTransform LeftHandTransform;
 };
